932ee8314fe244299364e373da24a0b12fcbd588
Qingyu Sui <sqybi@126.com>
2022-11-17 18:28:57 +0800

    First version

---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM continuumio/miniconda3 as spleeter
+
+WORKDIR /root
+ENV LANG C.UTF-8
+ENV LC_ALL C.UTF-8
+
+# Update Conda and install Conda packages
+RUN \
+    conda update -y -n base -c defaults conda && \
+    conda install -y -c conda-forge ffmpeg libsndfile
+
+# Install pip and install spleeter
+RUN \
+    python -m ensurepip --upgrade && \
+    pip3 install --upgrade pip && \
+    pip3 install spleeter
+
+# Download spleeter's models by processing example audio
+RUN \
+    wget https://github.com/deezer/spleeter/raw/master/audio_example.mp3 && \
+    spleeter separate -p spleeter:2stems -o output audio_example.mp3 && \
+    rm audio_example.mp3 && \
+    rm -r output
+
+
+FROM spleeter as frontend
+
+WORKDIR /root/frontend
+ENV NODE_VERSION=16.16.0
+ENV NVM_DIR=/root/.nvm
+ENV PATH="/root/.nvm/versions/node/v${NODE_VERSION}/bin/:${PATH}"
+
+# Install node
+RUN \
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && \
+    . "${NVM_DIR}/nvm.sh" && \
+    nvm install ${NODE_VERSION} && \
+    nvm alias default ${NODE_VERSION} && \
+    nvm use default
+
+# Build frontend project
+COPY frontend /root/frontend
+COPY supervisord.conf /etc/supervisord.conf
+
+RUN \
+    . "${NVM_DIR}/nvm.sh" && \
+    npm install
+
+EXPOSE 80
+ENTRYPOINT ["/root/.nvm/versions/node/v16.16.0/bin/node", "/root/frontend/main.js"]
---
+++ frontend/main.js
@@ -0,0 +1,51 @@
+import express from "express";
+import moment from "moment";
+import child_process from "node:child_process";
+import fs from "node:fs/promises";
+import http from "node:http";
+import path from "node:path";
+import url from "node:url";
+import util from "node:util";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {maxHttpBufferSize: 1e8 /* 100 MB */});
+const exec = util.promisify(child_process.exec);
+
+const data_dir = "/data";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function update_ext(old_path, new_ext) {
+  return old_path.substring(0, old_path.length - path.extname(old_path).length) + new_ext;
+}
+
+function rel(file_path) {
+  return path.relative(data_dir, file_path);
+}
+
+io.on("connection", async (socket) => {
+  socket.on("upload", async (file, file_name) => {
+    const date_str = moment().format("YYYYMMDD");
+    const target_dir = path.join(data_dir, date_str)
+    const target_path = path.join(target_dir, file_name);
+    await fs.mkdir(target_dir, {recursive: true});
+    await fs.writeFile(target_path, file);
+    socket.emit("uploaded", rel(target_path));
+
+    const mp3_path = target_path + ".mp3";
+    await exec(`ffmpeg -i "${target_path}" "${mp3_path}"`);
+    socket.emit("converted", rel(target_path), rel(mp3_path));
+
+    const output_dir = path.dirname(target_path);
+    await exec(`spleeter separate -p spleeter:2stems -o "${output_dir}" "${mp3_path}"`);
+    socket.emit("split", rel(target_path), rel(path.join(update_ext(mp3_path, ""), "vocals.wav")));
+  });
+});
+
+app.get('/', function (req, res) {
+  res.sendFile("index.html", {root: __dirname});
+});
+
+server.listen("80");
